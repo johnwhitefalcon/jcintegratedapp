f15a7cc686d368f34588b05c2bc857a4eb942adc
johnwhitefalcon <87903601+johnwhitefalcon@users.noreply.github.com>
2022-09-24 11:07:39 +1000

    Add files via upload

---
+++ posts.js
@@ -0,0 +1,10 @@
+import {name} from "../index"
+
+export default function handler(req, res) {
+    if (req.method === 'POST') {
+        const comment = req.body;
+        res.status(200).json(comment)
+    } 
+
+
+  }
